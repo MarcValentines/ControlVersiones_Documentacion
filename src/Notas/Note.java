e3273cc0201fbcc45dfbfa6feb623cffbd419eb3
Marc <marcvalentinesmiro@gmail.com>
2025-10-07 18:59:42 +0200

    Creación inicial del proyecto
    Se genera un proyecto de autodetección del tipo de notas según palabras clave

---
+++ src/Notas/Note.java
@@ -0,0 +1,25 @@
+package Notas;
+
+public class Note {
+    private String title;
+    private String content;
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Content: " + content);
+    }
+}
+
---
+++ src/Notas/NoteApp.java
@@ -0,0 +1,8 @@
+package Notas;
+
+public class NoteApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to SmartNotes!");
+        // Future logic here
+    }
+}
